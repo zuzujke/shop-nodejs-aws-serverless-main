660fe0881419671d84ac5654e74de44ce1defa97
Kostiantyn Semenenko <ksemenenko@delhaize.be>
2023-10-22 14:24:03 +0300

    Implement import service lambda

--- import-service/src/importProductsFile.ts
+++ import-service/src/importProductsFile.ts
@@ -1,22 +1,22 @@
 const BUCKET = process.env.BUCKET;
 import { errorResponse, successResponse } from "./utils/apiResponseBuilder";
 
-export const importProductsFile = ({
+export const importProductsFileHandler = ({
     s3,
 }) => async (event) => {
     try {
         const catalogName = event.queryStringParameters.name;
         const catalogPath = `uploaded/${catalogName}`;
-    
+
         const params = {
             Bucket: BUCKET,
             Key: catalogPath,
             Expires: 60,
             ContentType: 'text/csv'
         };
-    
+
         const url = await s3.getSignedUrlPromise('putObject', params)
-    
+
         return successResponse(url);
     } catch (error) {
         errorResponse(error);
